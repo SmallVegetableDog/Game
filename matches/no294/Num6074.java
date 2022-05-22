20b7afa3fa6462e81dfbf61feb5db457c15b6564
jinpeng <jinpeng@ideamake.cn>
2022-05-23 00:04:36 +0800

    dp

---
+++ matches/no294/Num6074.java
@@ -0,0 +1,45 @@
+package matches.no294;
+
+/**
+ * 6074. 字母在字符串中的百分比 显示英文描述
+ * 通过的用户数0
+ * 尝试过的用户数0
+ * 用户总通过次数0
+ * 用户总提交次数0
+ * 题目难度Easy
+ * 给你一个字符串 s 和一个字符 letter ，返回在 s 中等于 letter 字符所占的 百分比 ，向下取整到最接近的百分比。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "foobar", letter = "o"
+ * 输出：33
+ * 解释：
+ * 等于字母 'o' 的字符在 s 中占到的百分比是 2 / 6 * 100% = 33% ，向下取整，所以返回 33 。
+ * 示例 2：
+ * <p>
+ * 输入：s = "jjjj", letter = "k"
+ * 输出：0
+ * 解释：
+ * 等于字母 'k' 的字符在 s 中占到的百分比是 0% ，所以返回 0 。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 100
+ * s 由小写英文字母组成
+ * letter 是一个小写英文字母
+ */
+public class Num6074 {
+
+    public int percentageLetter(String s, char letter) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == letter) {
+                count++;
+            }
+        }
+        return count * 100 / s.length();
+    }
+}
