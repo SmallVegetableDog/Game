d781719dd800d1a13fc3cd5500ca3c570eb67f8b
jinpeng <jinpeng@ideamake.cn>
2022-06-19 21:45:53 +0800

    水平反转、主对角线翻转

---
+++ skill/Num48.java
@@ -0,0 +1,60 @@
+package skill;
+
+/**
+ * 48. 旋转图像
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * <p>
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[[7,4,1],[8,5,2],[9,6,3]]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+ * 输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * n == matrix.length == matrix[i].length
+ * 1 <= n <= 20
+ * -1000 <= matrix[i][j] <= 1000
+ */
+public class Num48 {
+
+    public static void main(String[] args) {
+        Num48 num48 = new Num48();
+//        num48.rotate(new int[][]{{1,2},{3,4}});
+        num48.rotate(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+
+    }
+
+    //水平翻转 再主对角线翻转
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int begin = 0, end = n - 1; begin < n / 2; begin++, end--) {
+            for (int j = 0; j < n; j++) {
+                int temp = matrix[begin][j];
+                matrix[begin][j] = matrix[end][j];
+                matrix[end][j] = temp;
+            }
+        }
+
+
+        for (int begin = 0; begin < n; begin++) {
+            for (int i = begin + 1; i < n; i++) {
+                int temp = matrix[begin][i];
+                matrix[begin][i] = matrix[i][begin];
+                matrix[i][begin] = temp;
+            }
+        }
+    }
+
+}
